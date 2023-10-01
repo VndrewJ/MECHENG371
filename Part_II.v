0fedc754e5190e1bbdd768ab39584d08420038d4
VndrewJ <109399808+VndrewJ@users.noreply.github.com>
2023-10-02 00:25:56 +1300

    Added counter for part 2

--- Part_II.v
+++ Part_II.v
@@ -1,63 +1,77 @@
-module Part_II(Counter, Data_A, Data_B,, Clock, data_out);
-	input [1:0]Counter;
-	input Clock;
+module Part_II(Data_A, Data_B, Clock, Data_out, reg_A_check, reg_B_check, counter_check);
 	input [7:0]Data_A, Data_B;
-	output [15:0]data_out;
+	input Clock;
 	
+	wire[15:0]sum;
 	wire [7:0]Reg_A, Reg_B;
-	wire [15:0]Reg_Sum;
+	wire [1:0]Counter;
 	wire ld;
+	
+	output [15:0]Data_out;
+	output [7:0] reg_A_check, reg_B_check;
+	output [1:0]counter_check;
+	
+	counter(Clock, Counter);
+	assign counter_check = Counter;
 	
 	assign ld = ~(Counter[1] | Counter[0]);
 	
-	Reg8bit RegA(Data_A, ld, Clock, Reg_A);
-	Reg8bit RegB(Data_B, ld, Clock, Reg_B);
-	bitAdder RegSum(Reg_A, Reg_B, Reg_Sum, Clock);
+	register(Data_A, Clock, ld, Reg_A);
+	register(Data_B, Clock, ld, Reg_B);
 	
-	Fourto1Mux mux(Reg_A, Reg_B, Reg_Sum, Clock, Counter, data_out);
+	assign reg_A_check = Reg_A;
+	assign reg_B_check = Reg_B;
 	
-endmodule 
+	adder(Reg_A, Reg_B, sum);
+	
+	mux(Reg_A, Reg_B, sum, Counter, Data_out);
+endmodule
 
-//mux
-module Fourto1Mux(Data_A, Data_B, Data_Sum, Clock, Counter, Data_out);
-	input [7:0]Data_A, Data_B;
-	input [15:0]Data_Sum;
-	input [1:0]Counter;
+//counter
+module counter(Clock, counter_out);
 	input Clock;
-	output reg [15:0]Data_out;
+	output reg [1:0]counter_out;
 	
 	always @(posedge Clock)
-		if (Counter == 2'b00)
-			Data_out <= 16'b0;
-		else if(Counter == 2'b01)
-			Data_out <= Data_A;
-		else if(Counter == 2'b10)
-			Data_out <= Data_B;
-		else if(Counter == 2'b11)
-			Data_out <= Data_Sum;
+		counter_out <= counter_out+1;
+
 endmodule
-			
-	
-//register
-module Reg8bit(Data_in, ld_sel, Clock, Data_out);
+
+//registers
+module register(Data_in, Clock, ld, Data_out);
 	input [7:0]Data_in;
-	input ld_sel, Clock;
+	input Clock, ld;
 	output reg [7:0]Data_out;
 	
 	always @(posedge Clock)
-		if(ld_sel == 1)
+		if(ld)
 			Data_out <= Data_in;
-endmodule		
-
+endmodule
 
 //adder
-module bitAdder(Data_A, Data_B, Data_out, Clock);
+module adder(Data_A, Data_B, Data_out);
 	input [7:0]Data_A, Data_B;
-	input Clock;
 	output reg [15:0]Data_out;
 	
-	always @(posedge Clock)
+	always @(*)
 		Data_out <= Data_A+Data_B;
+		
 endmodule
+
+//mux
+module mux(Data_A, Data_B, Data_Sum, Counter, Data_Out);
+	input [7:0]Data_A, Data_B;
+	input [15:0]Data_Sum;
+	input [1:0]Counter;
+	output reg [15:0]Data_Out;
 	
-	
+	always @(*)
+		case(Counter)
+			2'b00: Data_Out <= 0;
+			2'b01: Data_Out <= Data_A;
+			2'b10: Data_Out <= Data_B;
+			2'b11: Data_Out <= Data_Sum;
+			
+		endcase
+		
+endmodule
